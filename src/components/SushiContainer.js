deadefc2fe6ebad67703ebe95c18b18e5b628dae
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-08 17:00:13 +0300

    Code Deliverables Achieved

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+const API = "http://localhost:3001/sushis";
+
+function SushiContainer() {
+  const [sushiList, setSushiList] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const [budget, setBudget] = useState(100);
+
+  useEffect(() => {
+    fetch(API)
+      .then((response) => response.json())
+      .then((data) => setSushiList(data))
+      .catch((error) => console.error("Error fetching sushi: ", error));
+  }, []);
+
+  const handleMoreSushi = () => {
+    setStartIndex((prevIndex) => prevIndex + 4);
+  };
+
+  const handleEatSushi = (sushi) => {
+    if (budget >= sushi.price) {
+      const updatedSushiList = sushiList.map((s) =>
+        s.id === sushi.id ? { ...s, eaten: true } : s
+      );
+      setSushiList(updatedSushiList);
+      setBudget((prevBudget) => prevBudget - sushi.price);
+    }
+  };
+
+  const displayedSushi = sushiList.slice(startIndex, startIndex + 4);
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {displayedSushi.map((sushi) => (
+        <Sushi key={sushi.id} sushi={sushi} onEatSushi={handleEatSushi} />
+      ))}
+      <MoreButton onClickMore={handleMoreSushi} />
     </div>
   );
 }
